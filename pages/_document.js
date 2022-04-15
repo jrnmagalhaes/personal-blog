dd870f9e351f54780ac1a4595404c623ce97b9bf
Ricardo Nogueira <seuze.rick@gmail.com>
2022-04-15 00:11:23 -0300

    adiciona um document personalizado para tentar adicionar uma nova fonte

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" href="https://use.typekit.net/mda1qok.css"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
